8f281c54744a4d1995c059f0c1d81c682e0a8eae
StevenKitavi <steven.kitavi@gmail.com>
2019-07-30 23:18:03 +0300

    Added yaml dictinary

--- AutomationTalk/pyaml.py
+++ AutomationTalk/pyaml.py
@@ -5,4 +5,13 @@ def ly(filename):
     with open(filename) as _:
         return yaml.load(_)
 
-print(ly('1-list.yml'))
+#print the list in the 1-list.yml 
+my_list = ly('1-list.yml')
+print(my_list)
+
+
+#print the dictionary.yml using the python script
+my_dict = ly('dictionary.yml')
+for k, v in my_dict.items():
+    print v
+
